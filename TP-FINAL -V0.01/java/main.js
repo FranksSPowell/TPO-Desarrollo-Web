c0362e0f9a4acdfb442b679ac2f678232c052143
LGimem <laragimenezm@gmail.com>
2022-10-18 17:37:45 -0300

    validación de form con JS, y mejorando responsive del footer

--- TP-FINAL -V0.01/java/main.js
+++ TP-FINAL -V0.01/java/main.js
@@ -20,6 +20,51 @@ telf.onkeyup = function(e) {
 	}
 }
 
+//Enviar formulario
+
+console.log("funcionando")
+const formulario = document.getElementById("formulario");       //formulario
+const userName = document.getElementById("formExample1");       //nombre y apellido
+const userEmail = document.getElementById("form4Example2");     //email
+const userTelf = document.getElementById("telf");               //telefono
+
+const regUserName = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/;
+const regUserEmail = /^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$/;
+
+const mostrarMensajeExito = () => {
+    alertSuccess.classList.remove("d-none");
+    alertSuccess.textContent = "Mensaje enviado con éxito";
+    alertSuccess.style.background = "Green";
+    alertSuccess.style.color = "White";
+};
+const mostrarMensajeSoloLetras = () => {
+    alertSuccess.classList.remove("d-none");
+    alertSuccess.textContent = "El nombre solo puede tener letras";
+    alertSuccess.style.background = "Red";
+    alertSuccess.style.color = "White";
+};
+
+
+
+
+
+formulario.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    
+
+    if (!regUserName.test(userName.value)) {
+        console.log("Solo letras");
+        return mostrarMensajeSoloLetras();
+    }
+
+    
+    
+    mostrarMensajeExito()
+    console.log("Formulario enviado con éxito");
+    console.log("Formulario enviado");
+
+});
 
 
 
