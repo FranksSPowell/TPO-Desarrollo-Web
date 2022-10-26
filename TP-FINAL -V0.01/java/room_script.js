b8b6b4eaa0953fe1b2e69f5fc5c159e44050be00
FranksSPowell <101218360+FranksSPowell@users.noreply.github.com>
2022-10-25 22:06:53 -0300

    Add files via upload

--- TP-FINAL -V0.01/java/room_script.js
+++ TP-FINAL -V0.01/java/room_script.js
@@ -1,8 +1,8 @@
-let RoomDetails=[["H1_CAR1","H1_CAR2","H1_CAR3","H1_CAR4","H1_CAR5","H1_CAR6","H1_CAR7"],
-                ["H2_CAR1","H2_CAR2","H2_CAR3","H2_CAR4","H2_CAR5","H2_CAR6","H2_CAR7","H2_CAR8","H2_CAR9","H2_CAR10"],
-                ["H3_CAR1","H3_CAR2","H3_CAR3","H3_CAR4","H3_CAR5","H3_CAR6","H3_CAR7","H3_CAR8","H3_CAR9","H3_CAR10","H3_CAR11","H3_CAR12"],
-                ["H4_CAR1","H4_CAR2","H4_CAR3","H4_CAR4","H4_CAR5","H4_CAR6","H4_CAR7","H4_CAR8","H4_CAR9","H4_CAR10","H4_CAR11","H4_CAR12","H4_CAR13","H4_CAR14"],
-                ["H5_CAR1","H5_CAR2","H5_CAR3","H5_CAR4","H5_CAR5","H5_CAR6","H5_CAR7","H5_CAR8","H5_CAR9","H5_CAR10","H5_CAR11","H5_CAR12","H5_CAR13","H5_CAR14","H5_CAR15","H5_CAR16"]];
+let RoomDetails=[["Minibar","Bañera","Baño privado","Caja fuerte","Calefacción","TV de pantalla plana","Ropa de cama"],
+                ["Minibar","Bañera","Baño privado","Caja fuerte","Calefacción","TV de pantalla plana","Ropa de cama","Wifi","Pantuflas","parking"],
+                ["Minibar","Bañera","Baño privado","Caja fuerte","Calefacción","TV de pantalla plana","Ropa de cama","Wifi","Pantuflas","parking","Teléfono"," Aire acondicionado"],
+                ["Minibar","Bañera","Baño privado","Caja fuerte","Calefacción","TV de pantalla plana","Ropa de cama","Wifi","Pantuflas","parking","Teléfono"," Aire acondicionado","Sofa","Secador de pelo"],
+                ["Minibar","Bañera","Baño privado","Caja fuerte","Calefacción","TV de pantalla plana","Ropa de cama","Wifi","Pantuflas","parking","Teléfono"," Aire acondicionado","Sofa","Secador de pelo","jacuzzi","Robot asistente"]];
 
 
 console.log(RoomDetails[0][7]);
